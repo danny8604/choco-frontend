fadb54dbbf08e0c1151ccddc58431105b3776920
Danny <zx320320@example.com>
2023-01-31 01:39:24 +0800

    add oAuth google login modal message

--- src/app/hooks/useAuth.ts
+++ src/app/hooks/useAuth.ts
@@ -45,6 +45,12 @@ const useAuth = () => {
         const data = response.data;
 
         dispatch(
+          openUtilModal({
+            message: "Logged in success.",
+            isSucceed: true,
+          })
+        );
+        dispatch(
           userLogin({
             userEmail: data.user.email,
             userId: data.user.userId,

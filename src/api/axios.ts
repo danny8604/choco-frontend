4a29522965ac0c78af0d1a5147507391918d0343
Danny <zx320320@example.com>
2023-02-08 14:23:49 +0800

    modify baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,8 +5,8 @@ export interface tokenAndProductId {
   productId: string;
 }
 
-// export const baseURL = "https://choco-chair-backend.herokuapp.com/";
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://choco-chair-backend.herokuapp.com/";
+// export const baseURL = "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: baseURL,
